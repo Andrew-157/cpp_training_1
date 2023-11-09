a60b4160eff0b38f3376f073ecd4d0330d85750b
Andrew Subotin <subotinandrey5@gmail.com>
2023-11-09 11:37:35 +0100

    Exercise with variables 1

---
+++ variables_exercise_1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+// 1. Declare a variable named myInt of type int.
+// 2. Initialize myInt to a value of your choice.
+// 3. Declare a variable named myDouble of type double.
+// 4. Initialize myDouble to a value of your choice.
+
+int main()
+{
+    int myInt;
+    myInt = 23;
+
+    double myDouble;
+    myDouble = 23.4556;
+}
