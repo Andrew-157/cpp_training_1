acc1f84592ba7ceb52dda44740a50043f22d0e9d
Andrew Subotin <subotinandrey5@gmail.com>
2023-11-09 12:30:59 +0100

    Variables exercise 4

---
+++ variables_exercise_4.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+// 1. Declare a variable named myString of type std::string inside a block or scope of your choice.
+// 2. Assign myString a value of your choice.
+// 3. Print the value of myString to the console inside the same block or scope where it was declared.
+// 4. Attempt to print the value of myString to the console outside the block or scope where it was declared.
+// Observe the error message that is produced.
+
+int main()
+{
+    {
+        string myString = "String";
+        cout << myString << endl;
+    }
+    // Uncomment the following line and observe an error
+    // cout << myString << endl;
+}
