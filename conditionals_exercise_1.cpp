343c27ae619f057fc8ed7237ea55d0224ff97a1a
Andrew Subotin <subotinandrey5@gmail.com>
2023-11-11 12:27:07 +0100

    Update conditionals_exercise_1.cpp

--- conditionals_exercise_1.cpp
+++ conditionals_exercise_1.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 using namespace std;
 
+// 1. Prompt the user to enter a number between 1 and 5.
+// 2. Read the user input into an integer variable named choice.
+// 3. Write a switch statement that checks the value of choice.
+// If choice is 1, print the message "You chose option 1" to the console.
+// If choice is 2, print the message "You chose option 2" to the console.
+// If choice is 3, print the message "You chose option 3" to the console.
+// If choice is 4, print the message "You chose option 4" to the console.
+// If choice is 5, print the message "You chose option 5" to the console.
+// If choice is not between 1 and 5, print the message "Invalid choice" to the console.
+
 int main()
 {
     int choice;
